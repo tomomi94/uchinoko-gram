747747c2d5a4ecf52d2426c9f39d62c3e41f651a
tomomi94 <kurochan1004@gmail.com>
2021-04-15 13:45:16 +0900

    Add controller
    new/create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,14 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.order("created_at DESC")
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
   end
 
   private
